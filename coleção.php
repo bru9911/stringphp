cd9ff601702433293bac329a83fb50950a77ecde
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2022-12-18 18:42:38 -0300

    Create coleção.php

---
+++ coleção.php
@@ -0,0 +1,18 @@
+<?php
+$cores = array('vermelho', 'azul', 'verde', 'amarelo');
+
+foreach ($cores as $cor) {
+    echo "Você gosta de $cor?\n";
+}
+
+?>
+O exemplo acima irá imprimir:
+
+Você gosta de vermelho?
+Você gosta de azul?
+Você gosta de verde?
+Você gosta de amarelo?
+
+É possível alterar diretamente os valores de um array passando-os como referência.
+
+https://www.php.net/manual/pt_BR/language.types.array.php#example-58
