04d10be64246fce9b55c0c3068d57648ac631226
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2022-12-18 18:36:45 -0300

    Create chaves.php

---
+++ chaves.php
@@ -0,0 +1,24 @@
+<?php
+$array = array(
+         "a",
+         "b",
+    6 => "c",
+         "d",
+);
+var_dump($array);
+?>
+
+//output
+
+array(4) {
+  [0]=>
+  string(1) "a"
+  [1]=>
+  string(1) "b"
+  [6]=>
+  string(1) "c"
+  [7]=>
+  string(1) "d"
+}
+
+https://www.php.net/manual/pt_BR/language.types.array.php#example-58
